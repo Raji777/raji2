0594f1f6f1d10f6ab31901172ee24ae732c1c0ad
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-02-25 11:28:32 +0530

    even.cpp

---
+++ even.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"enter interval 1 and 2";
+cin>>a>>b;
+for(int i=a;i<b;i++)
+{
+if(i%2==0)
+{
+cout<<i<<endl;
+}
+}
+return 0;
+}
