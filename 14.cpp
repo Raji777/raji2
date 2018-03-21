5cd08df6105e49f55c6352e962fde4198a22b7c9
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-03-21 17:27:54 +0530

    14.cpp

---
+++ 14.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string.h>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+	char str[20],s[20];
+	int len, i, j;
+	cout<<"Enter a string : ";
+	cin>>str;
+	len=strlen(str);
+	for(i=0; i<len; i++)
+	{
+		if(str[i]=='a' || str[i]=='e' || str[i]=='i' ||
+		str[i]=='o' || str[i]=='u' || str[i]=='A' ||
+		str[i]=='E' || str[i]=='I' || str[i]=='O' ||
+		str[i]=='U')
+		{
+			for(j=i; j<len; j++)
+			{
+				str[j]=str[j+1];
+			}
+		len--;
+		}
+	}
+	cout<<"After deleting the vowels, the string will be : "<<str;
+	s=strrev(str);
+	cout<<s;
+	return 0;
+}
